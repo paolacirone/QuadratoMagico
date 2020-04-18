6069fc0dd2c1dc96c1c2695aa754eb31fd9cffc3
Lenovo Personal Pc <Lenovo Personal Pc@LAPTOP-M4HO8946.homenet.telecomitalia.it>
2020-04-18 11:46:06 +0200

    Soluzione.

--- src/main/java/it/polito/tdp/quadrato/model/RisolviQuadrato.java
+++ src/main/java/it/polito/tdp/quadrato/model/RisolviQuadrato.java
@@ -4,56 +4,120 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RisolviQuadrato {
-	private int N ; // lato del quadrato
-	private int N2 ; // numero di caselle (N^2)
-	private int magica ; // costante magica
-	
-	
+	private int N; // lato del quadrato
+	private int N2; // numero di caselle (N^2)
+	private int magica; // costante magica
+
+	private List<List<Integer>> soluzioni;
+	/*
+	 * Costruttore
+	 */
+	public RisolviQuadrato(int N) {
+		this.N = N;
+		this.N2 = N * N;
+		this.magica = N * (N2 + 1) / 2;
+	}
+
+	// calcola tutti i quadrati magici
+	public List<List<Integer>> quadrati() {
+
+		List<Integer> parziale = new ArrayList<>();
+
+		int livello = 0;
+		
+		this.soluzioni= new ArrayList<List<Integer>>();
+		this.cerca(parziale, livello);
+		
+		return soluzioni;
+		
+
+	}
+
+	// procedura ricorsiva(privata)
+	private void cerca(List<Integer> parziale, int livello) {
+
+		if (livello == N2) { // se sto riempendo l'ultima casella sono al caso terminale
+			// caso terminale
+			if (this.controlla(parziale)) {
+				System.out.println(parziale);
+				this.soluzioni.add(new ArrayList<>(parziale));   //devo farne una copia e non copiare il riferimento 
+			}
+			
+			return;
+		}
+
+		//controlli intermedi, quando il livello è multiplo di N (righe complete)
+		if(livello%N==0 && livello !=0) { //in questo caso se il livello è multiplo di N
+			if(!controllaRiga(parziale,livello/N-1))
+			return; //potatura dell'albero di ricerca
+		}
+		
+		
+		// caso intermedio
+		for (int valore = 1; valore <= N2; valore++) {
+			if (!parziale.contains(valore)) {
+				// se la sol. parziale contiene il valore non lo posso più usare
+				parziale.add(valore);
+				// ricorsione
+				this.cerca(parziale, livello + 1);
+				parziale.remove(parziale.size() - 1); // rimuovo l'ultimo elemento
+			}
+		}
+	}
+
+	private boolean controllaRiga(List<Integer> parziale, int i) {
+		int somma = 0; 
+		for(int col=0; col<N; col++)
+			somma+=parziale.get(i*N+col);
+		return somma==magica; //se sono uguali ritorna vero altrimenti falso
+	}
+
 	/**
 	 * Verifica se una soluzione rispetta tutte le somme
+	 * 
 	 * @param parziale
 	 * @return
 	 */
 	private boolean controlla(List<Integer> parziale) {
-		if(parziale.size()!=this.N*this.N)
-			throw new IllegalArgumentException("Numero di elementi insufficiente") ;
-		
+		if (parziale.size() != this.N * this.N)
+			throw new IllegalArgumentException("Numero di elementi insufficiente");
+
 		// Fai la somma delle righe
-		for(int riga=0; riga<this.N; riga++) {
-			int somma = 0 ;
-			for(int col=0; col<this.N; col++) {
-				somma += parziale.get(riga*this.N+col) ;
+		for (int riga = 0; riga < this.N; riga++) {
+			int somma = 0;
+			for (int col = 0; col < this.N; col++) {
+				somma += parziale.get(riga * this.N + col);
 			}
-			if(somma!=this.magica)
-				return false ;
+			if (somma != this.magica)
+				return false;
 		}
-		
+
 		// Fai la somma delle colonne
-		for(int col=0; col<this.N; col++) {
-			int somma = 0 ;
-			for(int riga=0; riga<this.N; riga++) {
-				somma += parziale.get(riga*this.N+col) ;
+		for (int col = 0; col < this.N; col++) {
+			int somma = 0;
+			for (int riga = 0; riga < this.N; riga++) {
+				somma += parziale.get(riga * this.N + col);
 			}
-			if(somma!=this.magica)
-				return false ;
+			if (somma != this.magica)
+				return false;
 		}
-		
+
 		// diagonale principale
 		int somma = 0;
-		for(int riga=0; riga<this.N; riga++) {
-			somma += parziale.get(riga*this.N+riga) ;
+		for (int riga = 0; riga < this.N; riga++) {
+			somma += parziale.get(riga * this.N + riga);
 		}
-		if(somma!=this.magica)
-			return false ;
-		
+		if (somma != this.magica)
+			return false;
+
 		// diagonale inversa
 		somma = 0;
-		for(int riga=0; riga<this.N; riga++) {
-			somma += parziale.get(riga*this.N+(this.N-1-riga)) ;
+		for (int riga = 0; riga < this.N; riga++) {
+			somma += parziale.get(riga * this.N + (this.N - 1 - riga));
 		}
-		if(somma!=this.magica)
-			return false ;
+		if (somma != this.magica)
+			return false;
 
-		return true ;
+		return true;
 	}
 }
--- src/main/java/it/polito/tdp/quadrato/model/TestRisolviQuadrato.java
+++ src/main/java/it/polito/tdp/quadrato/model/TestRisolviQuadrato.java
@@ -5,10 +5,10 @@ import java.util.List;
 public class TestRisolviQuadrato {
 
 	public static void main(String args[]) {
-		RisolviQuadrato r = new RisolviQuadrato(3) ;
-		List<List<Integer>> soluzioni = r.risolvi() ;
-		for(List<Integer> sol: soluzioni)
-			System.out.println(sol) ;
+		RisolviQuadrato r = new RisolviQuadrato(4) ;
+		List<List<Integer>> soluzioni = r.quadrati();
+	for(List<Integer> sol: soluzioni)
+		System.out.println(sol) ;
 	}
 	
 }
